b9efcd22b072a9164b8e8b51f8380ba03c177aee
Sravya2007 <sravyamarepally@outlook.com>
2020-06-14 19:25:17 +0530

    change in net and ball positions

--- sketch.js
+++ sketch.js
@@ -6,10 +6,12 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Render = Matter.Render;
 
-var ball1;
+var ball1, ball2;
 var net1, net2, net3, 
 net4, net5, net6, 
-net7, net8;
+net7, net8, net9,
+net10, net11, net12,
+net13;
 
 var ground1;
 
@@ -26,23 +28,34 @@ function setup() {
 
 	ground1 = new Ground(width/2, 680, width, 20);
 
-	net1 = new net(1300, 620, 100, 100);
+	net1 = new net(200, 620, 100, 100);
 
-	net2 = new net(700, 560, 100, 100);
+	net2 = new net(400, 540, 100, 100);
 
-	net3 = new net(1020, 360, 100, 100);
+	net3 = new net(600, 460, 100, 100);
 
-	net4 = new net(220, 160, 100, 100);
+	net4 = new net(800, 380, 100, 100);
 
-    net5 = new net(400, 360, 100, 100);
+    net5 = new net(1000, 300, 100, 100);
 
-	net6 = new net(700, 260, 100, 100);
+	net6 = new net(1200, 220, 100, 100);
 	
-	net7 = new net(1200, 160, 100, 100);
+	net7 = new net(1400, 140, 100, 100);
 	
-	net8 = new net(1400, 400, 100, 100);
+	net8 = new net(1400, 620, 100, 100);
+
+	net9 = new net(1200, 540, 100, 100);
+
+	net10 = new net(1000, 460, 100, 100);
+
+	net11 = new net(600, 300, 100, 100);
+
+	net12 = new net(400, 220, 100, 100);
+
+	net13 = new net(200, 140, 100, 100);
 
 	ball1 = new ball(530, 595, 20);
+	ball2 = new ball(1050, 595, 20);
 
 
 	var render = Render.create({
@@ -79,8 +92,20 @@ function draw() {
   net7.display();
  
   net8.display();
+
+  net9.display();
+
+  net10.display();
+
+  net11.display();
+
+  net12.display();
+
+  net13.display();
  
   ball1.display();
+
+  ball2.display();
 
   drawSprites();
  
@@ -93,6 +118,14 @@ function keyPressed() {
 
 	if(keyCode === LEFT_ARROW) {
 		Matter.Body.applyForce(ball1.body, ball1.body.position, {x: -20, y:-20});
+	}
+
+	if(keyCode === UP_ARROW) {
+		Matter.Body.applyForce(ball2.body, ball2.body.position, {x: 20, y:-20});
+	}
+
+	if(keyCode === DOWN_ARROW) {
+		Matter.Body.applyForce(ball2.body, ball2.body.position, {x: -20, y:-20});
 	}
   
 }
